805fbbbef1ef6ea0e82d49134aca6d67c75fa317
Aaron Russell <aaron@gc4.co.uk>
2015-10-04 16:03:08 +0100

    Added manifesto page with Sharing UI.

--- assets/javascripts/application.js.coffee
+++ assets/javascripts/application.js.coffee
@@ -7,3 +7,7 @@ jQuery ($) ->
     $('body').toggleClass 'nav-active'
   $('[data-nav-overlay]').click (e) ->
     $('body').removeClass 'nav-active'
+
+  $('.mn-cta .button').click (e) ->
+    e.preventDefault()
+    window.open $(this).attr('href'), 'blank', 'width=640,height=480,left=500,top=300'
--- lib/helpers/site_helpers.rb
+++ lib/helpers/site_helpers.rb
@@ -9,16 +9,21 @@ module SiteHelpers
   end
 
   def meta_tag(sym)
-    %Q( <meta name="#{ sym.to_s }" content="#{ @item[sym] }">) if @item[sym]
+    %Q(<meta name="#{ sym.to_s }" content="#{ @item[sym] }">) if @item[sym]
+  end
+
+  def open_graph(sym)
+    %Q(<meta name="og:#{ sym.to_s }" content="#{ @item[:open_graph][sym] }">) if @item[:open_graph][sym]
   end
 
   def body_class(*args)
     args += @item.identifier.split('/').reject(&:empty?)
-    args.join(' ')
+    args.empty? ? 'home' : args.join(' ')
   end
 
   def ficon(sym, text = nil)
     type = sym.to_s.gsub(/_/, '-')
+    text = "&nbsp;&nbsp;#{ text }" unless text.nil?
     %Q(<span class="ficon fi-#{ type }">#{ text }</span>)
   end
 
@@ -26,4 +31,22 @@ module SiteHelpers
     ENV['RACK_ENV'] == 'production' && @item[:comments]
   end
 
+  def share_manifesto_link(network)
+    url, params = case network
+    when :facebook
+      ['http://www.facebook.com/sharer/sharer.php', {
+        t: 'I support the PushType manifesto',
+        u: 'http://www.pushtype.org' + @item.identifier,
+      }]
+    when :twitter
+      ['https://twitter.com/intent/tweet', {
+        text:     'I support the @pushtype manifesto: inspiring a new generation of #ruby developers with a new generation of CMS',
+        url:      'http://www.pushtype.org' + @item.identifier,
+        related:  'pushtype,push_code,aaronrussell'
+      }]
+    end
+    "#{ url }?#{ Rack::Utils.build_query params }"
+  end
+
 end
+
